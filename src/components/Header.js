4f01d6683b9ec7c60dbaf6f939c463d01fd4ad13
Ajinkya Bhilare <ajinkya0011@gmail.com>
2023-03-20 14:50:47 +0530

    router implementation

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,16 +1,36 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import { FaUserAlt } from "react-icons/fa";
+import { BsCart4 } from "react-icons/bs";
 import logo from "../assest/logo.jpg";
 const Header = () => {
   const mystyle = {
     width: "50px",
-   
   };
   return (
     <div>
       {/* desktop */}
-      <div className="fixed shadow-md w-full h-16 px-2 md:px-4">
-        <div className="flex items-center h-full">
-          <img src={logo} alt="" style={mystyle} />
+      <div className="fixed shadow-md w-full h-16 px-2 md:px-4 flex justify-between ">
+        <Link to={""}>
+          <div className="flex items-center h-full ">
+            <img src={logo} alt="" style={mystyle} />
+          </div>
+        </Link>
+
+        <div className="flex gap-4 items-center">
+          <nav className="flex gap-5 items-center text-slate-400">
+            <Link to={""}>Home</Link>
+            <Link to={"menu"}>Menu</Link>
+            <Link to={"about"}>About </Link>
+            <Link to={"contact"}>Contact</Link>
+          </nav>
+          <div className="text-2xl text-slate-400">
+            <BsCart4 />
+          </div>
+
+          <div className="text-2xl text-slate-400">
+            <FaUserAlt />
+          </div>
         </div>
       </div>
 
--- src/index.js
+++ src/index.js
@@ -3,13 +3,28 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-import { createBrowserRouter } from "react-router-dom";
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
+} from "react-router-dom";
+import Home from "./page/Home";
+import Menu from "./page/Menu";
+import About from "./page/About";
+import Contact from "./page/Contact";
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<App />}>
+      <Route index element={<Home />}></Route>
+      <Route path="menu" element={<Menu />}></Route>
+      <Route path="about" element={<About />}></Route>
+      <Route path="contact" element={<Contact />}></Route>
+    </Route>
+  )
 );
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<RouterProvider router={router}></RouterProvider>);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
